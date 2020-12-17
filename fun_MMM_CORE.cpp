a0ee2c416a5bd17bf4f740a5675eb363b8ec450a
thttnn <matheus.vianna@ppge.ie.ufrj.br>
2020-12-17 20:33:41 +0100

    merged from capitaldemand

--- fun_MMM_CORE.cpp
+++ fun_MMM_CORE.cpp
@@ -9,31 +9,28 @@ MODELBEGIN
 
 /*****EQUATION FILES*****/
 
-#include "mmm_core_equations_time_step.h"          					// Time Step Variable
-#include "mmm_core_equations_initialization.h"               		// Initialization Variables
-#include "mmm_core_equations_functions.h"          					// Macro Functions
-#include "mmm_core_equations_macro.h"              					// Macro Object Variables and Functions
-#include "mmm_core_equations_macro_sfc.h"          					// Macro Object Variables for Stock-Flow Consistency
-	#include "mmm_core_equations_classes.h"            				// Income Classes Object Variables
-	#include "mmm_core_equations_government.h"         				// Government Object Variables
-	#include "mmm_core_equations_external_sector.h"    				// External Sector Object Variables
-		#include "mmm_core_equations_entry_exit.h"       			// Entry and Exit Variables (inside Sector Object)
-		#include "mmm_core_equations_sector_external.h"				// Sector Variables related to External Sector
-		#include "mmm_core_equations_sector_demand.h"				// Sector Variables for Demand Calculations
-		#include "mmm_core_equations_sector_aggregates.h"			// Sector Agreggates and Averages Variables
-			#include "mmm_core_equations_firm_rnd.h"				// Firm's R&D Variables			
-			#include "mmm_core_equations_firm_production.h"			// Firm's Production Variables
-			#include "mmm_core_equations_firm_investment.h"			// Firm's Investment Variables
-			#include "mmm_core_equations_firm_sales.h"				// Firm's Sales Variables
-			#include "mmm_core_equations_firm_price.h"				// Firm's Price Variables
-			#include "mmm_core_equations_firm_profit.h"				// Firm's Profit Variables
-			#include "mmm_core_equations_firm_capital.h"			// Firm's Capital Variables
-			#include "mmm_core_equations_firm_finance.h"			// Firm's Finance Variables
-			#include "mmm_core_equations_firm_inputs.h" 			// Firm's Input Variable      
-				#include "mmm_core_equations_capital_goods.h"  		// Capital Goods Object Variables
-//#include "mmm_core_equations_analysis.h"           				// Variables for Analysis
-#include "mmm_core_equations_analysis_2.h"           				// Variables for Analysis Long
-#include "mmm_core_equations_sector_analysis.h"						// Sector Variables for Analysis
+#include "mmm_core_time_step.h"          					// Time Step Variable
+#include "mmm_core_calibration.h"               			// Calibration calculation
+#include "mmm_core_macro.h"              					// Macro Object Variables and Functions
+#include "mmm_core_macro_sfc.h"          					// Macro Object Variables for Stock-Flow Consistency
+	#include "mmm_core_classes.h"            				// Income Classes Object Variables
+	#include "mmm_core_government.h"         				// Government Object Variables
+	#include "mmm_core_external_sector.h"    				// External Sector Object Variables
+		#include "mmm_core_entry_exit.h"       				// Entry and Exit Variables (inside Sector Object)
+		#include "mmm_core_sector_demand.h"					// Sector Variables for Demand Calculations
+		#include "mmm_core_sector_aggregates.h"				// Sector Agreggates and Averages Variables
+			#include "mmm_core_firm_rnd.h"					// Firm's R&D Variables			
+			#include "mmm_core_firm_production.h"			// Firm's Production Variables
+			#include "mmm_core_firm_investment.h"			// Firm's Investment Variables
+			#include "mmm_core_firm_sales.h"				// Firm's Sales Variables
+			#include "mmm_core_firm_price.h"				// Firm's Price Variables
+			#include "mmm_core_firm_profit.h"				// Firm's Profit Variables
+			#include "mmm_core_firm_capital.h"				// Firm's Capital Variables
+			#include "mmm_core_firm_finance.h"				// Firm's Finance Variables
+			#include "mmm_core_firm_inputs.h" 				// Firm's Input Variable      
+				#include "mmm_core_capital_goods.h"  		// Capital Goods Object Variables
+#include "mmm_core_analysis.h"           					// Variables for Analysis
+#include "mmm_core_sector_analysis.h"						// Sector Variables for Analysis
 
 MODELEND
 
